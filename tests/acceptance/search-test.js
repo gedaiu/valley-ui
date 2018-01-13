d3078b5469019145f324aaca9a93eb46eec0036a
Szabo Bogdan <bogdan.szabo@altom.ro>
2018-01-13 22:18:51 +0200

    add a mock search bar

---
+++ tests/acceptance/search-test.js
@@ -0,0 +1,31 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'valley-ui/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | search');
+
+test('should show search as the home page', function (assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/search', 'should redirect automatically');
+  });
+});
+
+test('should display the search bar', function (assert) {
+  visit('/search');
+  andThen(() => {
+    alert(find('.search .query').length);
+    assert.equal(find('.search .query').length, 1)
+    assert.equal(find('.search .btn').length, 1)
+  });
+});
+
+test('should list the query results', function (assert) {
+});
+
+test('visiting /search', function(assert) {
+  visit('/search');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/search');
+  });
+});
